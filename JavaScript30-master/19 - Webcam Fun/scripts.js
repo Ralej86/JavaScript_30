14a6ad673980c060b368cab9a2649aba48772ebe
Ryan <alejandr257@gmail.com>
2018-10-04 13:23:37 -0700

    Get video working

--- JavaScript30-master/19 - Webcam Fun/scripts.js
+++ JavaScript30-master/19 - Webcam Fun/scripts.js
@@ -4,6 +4,22 @@ const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
 
-function getvideo() {
-  
+function getVideo() {
+  navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+    .then(localMediaStream => {
+      console.log(localMediaStream);
+      video.srcObject = localMediaStream;
+      video.play();
+    })
+    .catch( err => {
+      console.error('Error alert:', err);
+    });
 }
+
+function paintToCanvas() {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+
+}
+
+getVideo();
